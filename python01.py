cea409e3a97feb9869324cc957fecb571ec42573
songjeonghyun11 <46089018+songjeonghyun11@users.noreply.github.com>
2020-05-19 17:49:21 +0900

    Create python01.py

---
+++ python01.py
@@ -0,0 +1,72 @@
+# 정수형
+
+a = 1
+b = 2
+c = a + b
+print(c)
+
+d = a * b
+print(d)
+
+e = a / b
+print(e)
+
+# 실수형
+a = 1.1
+b = 2.2
+c = a+b
+print(c)
+
+d = a*b
+print(d)
+
+e = a/b
+print(e)
+
+a = 'hel'
+b= 'lo'
+c = a+b
+print(c)
+
+a= 123
+b= '45'
+#c = a+b #타입에러 
+#print(c)
+
+#숫자를 문자변환  + 문자
+a = 123
+b = '45'
+a = str(a)
+print(a)
+c = a + b
+print(c)
+
+a = 123
+b = '45'
+b = int(b)
+c = a + b
+print(c)
+
+#문자열 연산하기 
+a = 'abcdefgh'
+print(a[0])
+print(a[3])
+print(a[5])
+print(a[-1])
+print(a[-2])
+print(type(a))
+
+b = 'xyz'
+print(a + b)
+
+#문자열 인덱싱
+a = 'hello, Deep learning'
+
+print(a[7])
+print(a[-1])
+print(a[-2])
+print(a[3:9])
+print(a[3:-5])
+print(a[:-1])
+print(a[1:])
+print(a[5:-4])
