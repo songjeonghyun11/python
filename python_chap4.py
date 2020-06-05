da1f448d6eb9725b6a7558e99f5167fe561723b9
songjeonghyun11 <46089018+songjeonghyun11@users.noreply.github.com>
2020-06-05 19:53:11 +0900

    Create python_chap4.py

---
+++ python_chap4.py
@@ -0,0 +1,55 @@
+#print("Hello world")
+
+#print(5 + 2) 
+#print(3 + 8)
+
+#print(3 + 6)
+#print("8 - 3")
+
+#print(18)
+#print(2 + 6)
+#print("2 + 6")
+
+#print(3 + 5)
+#print(3 - 5)
+#print(3 * 5)
+#print(3 / 5)
+#print(3 % 5)
+#print(3**5)
+
+#n = "강아지"
+#print(n)
+
+#m = "고양이"
+#print(m)
+#n = 3
+#n = n * 5
+#print(n)
+#m = "홍길동"
+#print(" 내이름은",  m , "입니다")
+
+#h = 1.7
+#w = 60
+
+#bmi = (w*w)/h
+#print(type(bmi))
+#print(str(bmi))
+
+#print( 4 + 6 != -10)
+
+#n = 16
+#if n > 15:
+#    print("n 은 15보다 큰숫자")
+
+# n = 13
+# if n < 15:
+#     print("15보다 작은 숫자")
+# else:
+#     print("15보다 큰 숫자")
+
+# year = 2500
+# if year % 100 == 0 and year % 400 != 0:
+#     print("윤년이 아닙니다")
+# elif year % 4 ==0:
+#     print("윤년입니다") 
+
